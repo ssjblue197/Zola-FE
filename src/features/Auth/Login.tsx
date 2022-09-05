5b852ba238c363297668ad5c289bc4c35f0934de
ssjblue197 <ssjblue197@gmail.com>
2022-09-06 00:14:01 +0700

    update

--- src/features/Auth/Login.tsx
+++ src/features/Auth/Login.tsx
@@ -2,7 +2,8 @@ import * as React from 'react';
 import { faUser, faQuestion } from "@fortawesome/free-solid-svg-icons";
 import { faGoogle, faFacebook, faTwitter} from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import authBG from "../../assets/bg.png"
+import logoApp from "../../assets/logo_transparent.png"
 
 export interface LoginProps {
 }
@@ -10,71 +11,79 @@ export interface LoginProps {
 export function Login (props: LoginProps) {
   return (
     <div className="flex flex-row w-screen h-screen">
-      <div className="basis-1/2 bg-slate-200 bg-scroll bg-[url('../../assets/bg.png')]">
+      <div className="basis-1/2 bg-slate-200 sm:basis-0">
         <img
-          className="w-full object-cover"
-          src="../../assets/bg.png"
+          className="w-full h-full object-contain"
+          src={authBG}
         />
       </div>
-      <div className="basis-1/2 justify-center place-items-center flex bg-slate-200">
-        <div className="flex flex-col justify-center items-center">
-          <div className="w-40 h-40 rounded-full bg-slate-400 flex justify-center items-center mb-10 shadow-xl ring-4">
-            <FontAwesomeIcon icon={faUser} 
+      <div className="basis-1/2 justify-between place-items-center flex-col flex bg-slate-200">
+        <span className="mb-10">
+        </span>
+        <div className="w-1/2 flex flex-col justify-center items-center">
+          <div className="w-40 h-40 rounded-full bg-slate-300 flex justify-center items-center mb-4 shadow-xl ring-4">
+            {/* <FontAwesomeIcon icon={faUser} 
               size="4x"
               className=""
               inverse
+            /> */}
+            <img
+              src={logoApp}
             />
           </div>
-          <div className="w-full px-10">
+          <div className="w-full flex flex-col">
             <div className="w-full mt-4 flex flex-row justify-between items-center">
-              <span className="text-gray-800 font-semibold text-lg mr-2 basis-1/5">Email</span>
-              <input placeholder="Your email" type="email" className="grow placeholder-slate-300 form-input px-3 py-3 rounded-xl bg-slate-500 border-none shadow-md caret-white text-gray-200"></input>
+              <span className="text-gray-600 font-semibold text-xl mr-2 basis-1/5 lg:invisible truncate ">Email</span>
+              <input placeholder="Your email" type="email" className="grow placeholder-slate-500 form-input px-3 py-3 rounded-xl bg-slate-300 border-none shadow-md caret-gray-500 text-gray-700 text-xl"></input>
             </div>
             <div className="w-full mt-4 flex flex-row justify-between items-center">
-              <span className="text-gray-800 font-semibold text-lg mr-2 basis-1/5">Password</span>
-              <input placeholder="Your password" type="password" className="grow placeholder-slate-300 form-input px-3 py-3 rounded-xl bg-slate-500 border-none shadow-md caret-white text-gray-200"></input>
+              <span className="text-gray-600 font-semibold text-xl mr-2 basis-1/5 lg:invisible truncate">Password</span>
+              <input placeholder="Your password" type="password" className="grow placeholder-slate-500 form-input px-3 py-3 rounded-xl bg-slate-300 border-none shadow-md caret-gray-500 text-gray-700 text-xl"></input>
             </div>
           </div>
-          <div className="w-full px-10 mt-10 flex flex-row justify-between">
-            <div>
+          <div className="w-full  mt-10 flex flex-row justify-between">
+            <div className="truncate">
               <input
                 type="checkbox"
-                className="cursor-pointer border-none outline-none"
+                className="cursor-pointer border-slate-400 ml-2"
               />
-              <span className="text-gray-800 font-light text-md ml-2">
+              <span className="text-gray-700 font-light text-lg ml-2">
                 Remember
               </span>
             </div>
-            <div>
-              <span className="text-gray-800 font-semibold text-md mx-2 cursor-pointer">
-                Forgot Password
-              </span>
-              <FontAwesomeIcon icon={faQuestion}
+            <span className="text-blue-500 font-semibold text-lg mx-2 cursor-pointer truncate">
+              Forgot Password?
+            </span>
+          </div>
+          <div className=" w-full mt-10">
+            <button className="btn-success w-full py-3 text-xl">Login</button>
+          </div>
+          <div className=" w-full mt-2 btn-danger flex place-content-center flex-row text-xl">
+            <button className="mr-2">
+              <FontAwesomeIcon icon={faGoogle} 
+                className=""
                 inverse
               />
-            </div>
+            </button>
+            <span className="truncate md:invisible">Signin with Google</span>
           </div>
-          <div className="w-full px-10 mt-10 flex flex-row justify-between">
-            <button className="w-32 btn-warning">Register</button>
-            <div>
-              <button className="btn-danger mr-2">
-                <FontAwesomeIcon icon={faGoogle} 
-                  size="2x"
-                  className=""
-                  inverse
-                />
-              </button>
-              <button className="btn-primary">
-                <FontAwesomeIcon icon={faFacebook} 
-                  size="2x"
-                  className=""
-                  inverse
-                />
-              </button>
-            </div>
-            <button className="w-32 btn-primary">Login</button>
+          <div className="btn-primary w-full mt-2 flex place-content-center flex-row text-xl">
+            <button className="mr-2">
+              <FontAwesomeIcon icon={faFacebook} 
+                className=""
+                inverse
+              />
+            </button>
+            <span className="truncate md:invisible">Signin with Facebook</span>
           </div>
         </div>
+        <span className="mb-10 text-xl text-gray-700">
+          Don't have an account yet? 
+          <span
+            className="font-semibold ml-2 text-blue-500 text-xl"
+          >Sign Up
+          </span>
+        </span>
       </div>
     </div>
   );
--- src/features/Auth/Register.tsx
+++ src/features/Auth/Register.tsx
@@ -1,12 +1,80 @@
 import * as React from 'react';
+import { faUser, faQuestion } from "@fortawesome/free-solid-svg-icons";
+import { faGoogle, faFacebook, faTwitter} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import authBG from "../../assets/bg.png"
+import logoApp from "../../assets/logo_transparent.png"
 
 export interface RegisterProps {
 }
 
 export function Register (props: RegisterProps) {
   return (
-    <div>
-      Register
+    <div className="flex flex-row w-screen h-screen">
+      <div className="basis-1/2 bg-slate-200 sm:basis-0">
+        <img
+          className="w-full h-full object-contain"
+          src={authBG}
+        />
+      </div>
+      <div className="basis-1/2 justify-between place-items-center flex-col flex bg-slate-200">
+        <span className="mb-10">
+        </span>
+        <div className="w-1/2 flex flex-col justify-center items-center">
+          <div className="w-40 h-40 rounded-full bg-slate-300 flex justify-center items-center mb-4 shadow-xl ring-4">
+            {/* <FontAwesomeIcon icon={faUser} 
+              size="4x"
+              className=""
+              inverse
+            /> */}
+            <img
+              src={logoApp}
+            />
+          </div>
+          <div className="w-full flex flex-col">
+            <div className="w-full mt-4 flex flex-row justify-between items-center">
+              <span className="text-gray-600 font-semibold text-xl mr-2 basis-1/5 lg:invisible truncate ">First Name</span>
+              <input placeholder="First name" type="text" className="grow placeholder-slate-500 form-input px-3 py-3 rounded-xl bg-slate-300 border-none shadow-md caret-gray-500 text-gray-700 text-xl"></input>
+            </div>
+            <div className="w-full mt-4 flex flex-row justify-between items-center">
+              <span className="text-gray-600 font-semibold text-xl mr-2 basis-1/5 lg:invisible truncate ">Last Name</span>
+              <input placeholder="First name" type="text" className="grow placeholder-slate-500 form-input px-3 py-3 rounded-xl bg-slate-300 border-none shadow-md caret-gray-500 text-gray-700 text-xl"></input>
+            </div>
+            <div className="w-full mt-4 flex flex-row justify-between items-center">
+              <span className="text-gray-600 font-semibold text-xl mr-2 basis-1/5 lg:invisible truncate ">Gender</span>
+              <select name="gender" id="gender" className="grow placeholder-slate-500 form-input px-3 py-3 rounded-xl bg-slate-300 border-none shadow-md caret-gray-500 text-gray-700 text-xl">
+                <option value="male" className="grop px-3 py-3 rounded-xl bg-slate-300 border-none shadow-md text-gray-700 text-xl">
+                  Male
+                </option>
+                <option value="female" className="grop px-3 py-3 rounded-xl bg-slate-300 border-none shadow-md text-gray-700 text-xl">
+                  Female
+                </option>
+              </select>
+            </div>
+            <div className="w-full mt-4 flex flex-row justify-between items-center">
+              <span className="text-gray-600 font-semibold text-xl mr-2 basis-1/5 lg:invisible truncate ">Birthday</span>
+              <input placeholder="Date of birth" type="date" className="grow placeholder-slate-500 form-input px-3 py-3 rounded-xl bg-slate-300 border-none shadow-md caret-gray-500 text-gray-700 text-xl"></input>
+            </div>
+            <div className="w-full mt-4 flex flex-row justify-between items-center">
+              <span className="text-gray-600 font-semibold text-xl mr-2 basis-1/5 lg:invisible truncate ">Email</span>
+              <input placeholder="Your email" type="email" className="grow placeholder-slate-500 form-input px-3 py-3 rounded-xl bg-slate-300 border-none shadow-md caret-gray-500 text-gray-700 text-xl"></input>
+            </div>
+            <div className="w-full mt-4 flex flex-row justify-between items-center">
+              <span className="text-gray-600 font-semibold text-xl mr-2 basis-1/5 lg:invisible truncate">Password</span>
+              <input placeholder="Your password" type="password" className="grow placeholder-slate-500 form-input px-3 py-3 rounded-xl bg-slate-300 border-none shadow-md caret-gray-500 text-gray-700 text-xl"></input>
+            </div>
+            <div className="w-full mt-4 flex flex-row justify-between items-center">
+              <span className="text-gray-600 font-semibold text-xl mr-2 basis-1/5 lg:invisible truncate">Repeat</span>
+              <input placeholder="Repeat your password" type="password" className="grow placeholder-slate-500 form-input px-3 py-3 rounded-xl bg-slate-300 border-none shadow-md caret-gray-500 text-gray-700 text-xl"></input>
+            </div>
+          </div>
+          <div className=" w-full mt-10">
+            <button className="btn-primary w-full py-3 text-xl">Register</button>
+          </div>
+        </div>
+        <span className="mb-10 text-xl text-gray-700">
+        </span>
+      </div>
     </div>
   );
 }
